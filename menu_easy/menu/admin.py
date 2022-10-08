349646b6cbd0edf263bd7b96361a8610d95c0ace
Axel Diaz <axtherocknass@gmail.com>
2022-10-08 15:51:05 -0500

    Creating models in Category, Products, Store

--- menu_easy/menu/admin.py
+++ menu_easy/menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from menu.models import Product, Category
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Category, CategoryAdmin)
--- menu_easy/menu/models.py
+++ menu_easy/menu/models.py
@@ -1,3 +1,25 @@
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length =50)
+    #priority = models.TimeField()
+    
+class Tienda(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField()
+    sloga = models.CharField(max_length = 400)
+    
+class Product(models.Model):
+    title = models.CharField(max_length = 50)
+    price = models.DecimalField(decimal_places = 3, max_digits = 6)
+    description = models.TextField()
+    service_init = models.TimeField()
+    servece_end = models.TimeField()
+    category = models.OneToOneField(
+        Category,
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return f'{self.title}'
